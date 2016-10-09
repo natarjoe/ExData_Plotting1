19e8577d586b77e28846aae6773196ca6da10a54
natarjoe <natarjoe@hotmail.com>
2016-10-09 14:43:31 -0400

    Add files via upload

--- Plot4.R
+++ Plot4.R
@@ -31,7 +31,7 @@ lines(cleandata$datetime, cleandata$Sub_metering_2, type="l", col="red")
 
 lines(cleandata$datetime, cleandata$Sub_metering_3, type="l", col="blue")
 
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty= 1, col=c("black", "red", "blue"), bty="o")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty= 1, col=c("black", "red", "blue"),bty="n")
 
 
 
